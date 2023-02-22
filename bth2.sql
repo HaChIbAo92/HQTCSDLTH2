b21e2d3ea94200ee5711289a86ab0fefc2a12099
HaChIbAo92 <118881798+HaChIbAo92@users.noreply.github.com>
2023-02-22 10:53:14 +0700

    Add files via upload

---
+++ bth2.sql
@@ -0,0 +1,189 @@
+﻿--2. Trong CSDL vừa tạo, sử dụng câu lệnh SQL để thiết kế cấu trúc các bảng có khóa chính, khóa ngoại và kiểu dữ liệu theo lược đồ sau:
+---Tạo database
+create database QLDA2
+go 
+use QLDA2
+go
+
+
+---Tạo bảng
+--Nhân viên
+create table NHANVIEN
+(
+MaNV varchar(9) NOT NULL,
+HoNV nvarchar(15),
+TenLot nvarchar(30),
+TenNV nvarchar(30),
+NgSinh smalldatetime,
+DiaChi nvarchar(150),
+Phai nvarchar(3),
+Luong numeric(18, 0),
+Phong varchar(2),
+)
+
+--Thân nhân
+create table THANNHAN
+(
+MaNV varchar(9) Not null,
+TenTN varchar(20) not null,
+NgaySinh smalldatetime,
+Phai varchar(3),
+Quanhe varchar(15),
+)
+
+--Đề án
+create table DEAN
+(
+MaDA varchar(2) not null,
+TenDA nvarchar(50),
+DDiemDA varchar(20),
+)
+
+--Phân công
+create table PHANCONG
+(
+MaNV varchar(9) not null,
+MaDA varchar(2) not null,
+ThoiGian numeric(18, 0),
+)
+
+--Phòng ban
+create table PHONGBAN
+(
+MaPhg varchar(2) not null,
+TenPhg nvarchar(20),
+)
+
+
+---Thiết kế khóa chính, khóa ngoại:
+--Nhân viên
+ALTER TABLE NHANVIEN ADD CONSTRAINT Pk_Nhanvien PRIMARY KEY (MaNV);
+ALTER TABLE NHANVIEN ADD CONSTRAINT FK_Pbnv FOREIGN KEY (Phong) REFERENCES PHONGBAN(MaPhg);
+
+--Thân nhân
+ALTER TABLE THANNHAN ADD CONSTRAINT FK_Thannhana FOREIGN KEY (MaNV) REFERENCES NHANVIEN(MaNV);
+ALTER TABLE THANNHAN ADD CONSTRAINT PK_Thannhanb PRIMARY KEY (TenTN);
+
+--Đê án
+ALTER TABLE DEAN ADD CONSTRAINT PK_Dean PRIMARY KEY (MaDA);
+
+--phân công
+ALTER TABLE PHANCONG ADD CONSTRAINT FK_Phanconga FOREIGN KEY (MaNV) REFERENCES NHANVIEN(MaNV);
+ALTER TABLE PHANCONG ADD CONSTRAINT FK_Phancongb FOREIGN KEY (MaDA) REFERENCES DEAN(MaDA);
+ALTER TABLE PHANCONG ADD CONSTRAINT PK_Phancongc PRIMARY KEY (MaNV,MaDA);
+
+--phòng ban
+ALTER TABLE PHONGBAN ADD CONSTRAINT PK_Phongban PRIMARY KEY (MaPhg);
+
+
+
+---4. Sử dụng câu lệnh SQL để thực hiện các yêu cầu sau:
+--- Trong bảng PHONGBAN, chỉnh sửa kiểu dữ liệu của cột TenPhg thành nvarchar(30)
+ALTER TABLE PHONGBAN
+ALTER COLUMN TenPhg NVARCHAR(30)
+
+--- Trong bảng DEAN, chỉnh sửa kiểu dữ liệu của cột DDiemDA thành nvarchar(20)
+ALTER TABLE DEAN
+ALTER COLUMN DDiemDA NVARCHAR(20)
+
+--- Trong bảng THANNHAN, chỉnh sửa kiểu dữ liệu của cột TenTN thành nvarchar(20), kiểu dữ liệu của cột Phai thành nvarchar(3), kiểu dữ liệu của cột QuanHe thành nvarchar(15)
+ALTER TABLE THANNHAN
+ALTER COLUMN TenTN NVARCHAR(20)
+ALTER TABLE THANNHAN
+ALTER COLUMN Phai NVARCHAR(3)
+ALTER TABLE THANNHAN
+ALTER COLUMN QuanHe NVARCHAR(15)
+
+---Trong bảng NHANVIEN, thêm cột SoDienThoai với kiểu dữ liệu varchar(15)
+ALTER TABLE NHANVIEN
+ADD SoDienThoai varchar(15)
+
+--- Trong bảng NHANVIEN, xóa cột SoDienThoai vừa tạo.
+ALTER TABLE NHANVIEN
+DROP column SoDienThoai
+
+
+---5. Sử dụng câu lệnh SQL để nhập liệu vào các bảng như sau:
+--Nhân viên:
+insert into NHANVIEN
+values ('123', N'Đinh', N'Bá', N'Tiến', '1982-2-27', 'Mộ Đức', 'Nam', '', '4')
+insert into NHANVIEN
+values ('234', N'Nguyễn', N'Thanh', N'Tùng', '1956-8-12', 'Sơn Tịnh', 'Nam', '', '5')
+insert into NHANVIEN
+values ('345', N'Bùi', N'Thúy', N'Vũ', 'null', 'Tư Nghĩa', N'Nữ', '', '4')
+insert into NHANVIEN
+values ('456', N'Lê', N'Thị', N'Nhàn', '1962-7-12', 'Mộ Đức', N'Nữ', '', '4')
+insert into NHANVIEN
+values ('567', N'Nguyễn', N'Mạnh', N'Hùng', '1985-3-25', 'Sơn Tịnh', 'Nam', '', '5')
+insert into NHANVIEN
+values ('678', N'Trần', N'Hồng', N'Quang', 'null', 'bình Sơn', 'Nam', '', '5')
+insert into NHANVIEN
+values ('789', N'Trần', N'Thanh', N'Tâm', '1972-6-17', 'Quãng Ngãi', 'Nam', '', '5')
+insert into NHANVIEN
+values ('890', N'Cao', N'Thanh', N'Huyền', 'null', 'Tư Nghĩa', N'Nữ', '', '1')
+insert into NHANVIEN
+values ('901', N'Vương', N'Ngọc', N'Quyền', '1987-12-12', 'Mộ Đức', N'Nam', '', '1')
+
+--thân nhân:
+insert into THANNHAN
+values ('123', N'Châu', '2005-10-30', N'Nữ', N'Con gái')
+insert into THANNHAN
+values ('123', N'Duy', '2001-10-25', N'Nam', N'Con trai')
+insert into THANNHAN
+values ('123', N'Phương', '1985-10-30', N'Nữ', N'Vợ chồng')
+insert into THANNHAN
+values ('234', N'Thanh', '1980-04-05', N'Nữ', N'Con gái')
+insert into THANNHAN
+values ('345', N'Dương', '1956-10-30', N'Nam', N'Vợ chồng')
+insert into THANNHAN
+values ('345', N'Khang', '1982-10-25', N'Nam', N'Con trai')
+insert into THANNHAN
+values ('456', N'Hùng', '1987-01-01', N'Nam', N'Con trai')
+insert into THANNHAN
+values ('901', N'Thương', '1984-04-05', N'Nữ', N'Vợ chồng')
+
+--Đê án:
+insert into DEAN
+values ('1', N'Nâng cao chất lượng muối', N'Sa Huỳnh')
+insert into DEAN
+values ('10', N'Xây dựng nhà máy chế biến thủy sản', N'Dung Quất')
+insert into DEAN
+values ('2', N'Phát triẻn hạ tầng mạng', N'Quảng Ngãi')
+insert into DEAN
+values ('20', N'Truyền tải cáp quang', N'Quảng Ngãi')
+insert into DEAN
+values ('3', N'Tin học hóa trường học', N'Ba Tơ')
+insert into DEAN
+values ('30', N'Đào tạo nhân lực', N'Tịnh Phong')
+
+--Phân công:
+insert into PHANCONG
+values ('123', '1', '30')
+insert into PHANCONG
+values ('123', '2', '8')
+insert into PHANCONG
+values ('345', '10', '10')
+insert into PHANCONG
+values ('345', '20', '10')
+insert into PHANCONG
+values ('345', '3', '10')
+insert into PHANCONG
+values ('456', '1', '20')
+insert into PHANCONG
+values ('456', '2', '20')
+insert into PHANCONG
+values ('678', '3', '40')
+insert into PHANCONG
+values ('789', '10', '35')
+insert into PHANCONG
+values ('789', '20', '30')
+insert into PHANCONG
+values ('789', '30', '5')
+
+--Phòng ban
+insert into PHONGBAN
+values ('1', N'Quản lý')
+insert into PHONGBAN
+values ('4', N'Điều hành')
+insert into PHONGBAN
+values ('5', N'Nghiên cứu')
---
+++ trainingI.sql
@@ -0,0 +1,81 @@
+﻿--I) Câu lệnh SELECT sử dụng các hàm thống kê với các mệnh đề Group by và Having
+--1) Liệt kê danh sách các hóa đơn (SalesOrderID) lặp trong tháng 6 năm 2008 có tổng tiền >70000, thông tin gồm SalesOrderID, Orderdate, SubTotal, trong đó SubTotal =sum(OrderQty*UnitPrice).
+select SOH.SalesOrderID, SOH.Orderdate, Sum(SOD.OrderQty * SOD.UnitPrice) as 'Sub Total'
+From Sales.SalesOrderHeader as SOH join Sales.SalesOrderDetail As SOD
+ON SOH.SalesOrderID = SOD.SalesOrderID
+Where month(soh.OrderDate) = 6 and year(soh.OrderDate) = 2008
+Group By SOH.SalesOrderID, SOH.OrderDate
+Having SUM(SOD.OrderQty * SOD.UnitPrice) > 70000
+
+--2) Đếm tổng số khách hàng và tổng tiền của những khách hàng thuộc các quốc gia có mã vùng là US (lấy thông tin từ các bảng SalesTerritory, Sales.Customer, Sales.SalesOrderHeader, Sales.SalesOrderDetail). Thông tin bao gồm TerritoryID, tổng số khách hàng (countofCus), tổng tiền (Subtotal) với Subtotal = SUM(OrderQty*UnitPrice)
+Select SST.TerritoryID, count(SC.CustomerID) as 'CountOfCust', SUM(SSOD.OrderQty * SSOD.UnitPrice) As 'Subtotal'
+from Sales.SalesTerritory as SST join Sales.Customer as SC
+ON SST.TerritoryID = SC.TerritoryID join Sales.SalesOrderHeader as SSOH
+ON SSOH.TerritoryID = SC.TerritoryID join Sales.SalesOrderDetail as SSOD
+ON SSOD.SalesOrderID = SSOH.SalesOrderID
+Where SST.CountryRegionCode = 'US'
+Group By SST.TerritoryID
+
+--3) Tính tổng trị giá của những hóa đơn với Mã theo dõi giao hàng (CarrierTrackingNumber) có 3 ký tự đầu là 4BD, thông tin bao gồm SalesOrderID, CarrierTrackingNumber, SubTotal=sum(OrderQty*UnitPrice)
+Select ssod.SalesOrderID, ssod.CarrierTrackingNumber, Sum(OrderQty*UnitPrice) as 'SubTotal'
+from sales.SalesOrderDetail as ssod
+group by ssod.SalesOrderID, ssod.CarrierTrackingNumber
+having ssod.CarrierkingNumber like '4BD%'
+
+--4) Liệt kê các sản phẩm (product) có đơn giá (unitPrice)<25 và số lượng bán trung bình >5, thông tin gồm ProductID, name, AverageofQty
+select pp.ProductID, pp.Name, AVG(ssod.UnitPrice) as 'AverageOfQty'
+from Production.Product as pp join sales.SalesOrderDetail as ssod
+ON pp.ProductID = ssod.ProductID
+Where ssod.Unitprice < 25
+group by pp.ProductID, pp.Name
+having avg(ssod.OrderQty) > 5
+
+--5) Liệt kê các công việc (JobTitle) có tổng số nhân viên >20 người, thông tin gồm JobTitle, countofPerson=count(*)
+select hre.JobTittle, count(hre.BusinessEntilyID) 'CountOfPerson'
+from HumanResources.Employee hre
+group by hre.JobTitle
+having count(hre.BusinessEntilyID) > 20
+
+--6) Tính tổng số lượng và tổng trị giá của các sản phẩm do các nhà cung cấp có tên kết thúc bằng ‘Bicycles’ và tổng trị giá >800000, thông tin gồm BusinessEntityID, Vendor_name, ProductID, sumofQty, SubTotal (sử dụng các bảng [Purchasing].[Vendor] [Purchasing].[PurchaseOrderHeader] và [Purchasing].[PurchaseOrderDetail])
+select pv.BusinessEntityId, pv.Name, ppod.ProductID,
+   SumOfQty = Sum(ppod.OrderQty), SubTotal = Sum(ppod.OrderQty * ppod.UnitPrice)
+from Purchasing .Vendor pv 
+     join Purchasing.PurchaseOrderHeader ppoh on pv.BusinessEntityId = ppoh.VendorId
+     join Purchasing.PurchaseOrderDetail ppoh on ppoh.PurchaseOrderId= ppoh.PurchaseOrderId
+Where pv.Name likes 'Bicycles' 
+group by pv.BusinessEntityId, pv.Name, ppod.ProductID
+having Sum(ppod.OrderQty * ppod.UnitPrice) > 800000
+
+--7) Liệt kê các sản phẩm có trên 500 đơn đặt hàng trong quí 1 năm 2008 và có tổng trị giá >10000, thông tin gồm ProductID, Product_name, countofOrderID và Subtotal
+select pp.Product.Id, pp.Name, CountOfOrderId = count(ssod.SalesOrderId), SubTotal Sum(ssod.Qty * ssod.UnitPrice )
+from Production.Product pp 
+join sales.SalesOrderDetail ssod on ssod.ProductId = pp.ProductId
+join sales SalesOrderHeadr ssoh on ssod.SalesOrderId = pp.SalesOrderId
+WHERE DATEPART(q, ssoh.orderdate) - 1 and year(ssoh .orderDate) -2008
+Group by pp.ProductId, pp.Name
+Having Sum(ssohd.OrderQty * ssod.UnitPrice)>10000 and count(ssod.SalesOrderId) > 500
+
+--8) Liệt kê danh sách các khách hàng có trên 25 hóa đơn đặt hàng từ năm 2007 đến 2008, thông tin gồm mã khách (PersonID) , họ tên (FirstName +' '+ LastName as fullname), Số hóa đơn (CountOfOrders).
+select sc.PersonId, FullName = (pp.FirtName + ''+pp.Last), Count OfOrders = count(ssoh.SalesOrderId)
+from Person.Person pp
+join Sales .Customer sc on pp.BusinessEntityId = sc.CustomerId
+Where year (ssoh .OrderDate) between 2007 and 2008
+group by sc.PersonId , pp.FirstName + '' + pp.LastName
+having count (ssohSalesOrderId)> 25
+
+--9) Liệt kê những sản phẩm có tên bắt đầu với ‘Bike’ và ‘Sport’ có tổng số lượng bán trong mỗi mỗi năm trên 500 sản phẩm, thông tin gồm ProductID, Name, CountofOrderQty, year. (dữ liệu lấy từ các bảng Sales.SalesOrderHeader, Sales.SalesOrderDetail, and Production.Product)
+select pp.ProductID, pp.Name, CountOfOderQty = sum(ssod.OrderQty), YearOfSale = year(ssoh.Orderdate)
+from Production.Product pp
+	join Sales.SalesOrderdetail ssod on ssod.ProductID = pp.ProductID
+	join Sales.SalesOrderHeader ssoh on ssod.SalesOrderID = ssoh.SalesOrderID
+where pp.Name like '%Bike' or pp.Name like '%Sport'
+group by pp.ProductID, pp.name, year(ssoh.OrderDate)
+having sum(ssod.OrderQty) > 50
+
+--10)Liệt kê những phòng ban có lương (Rate: lương theo giờ) trung bình >30, thông tin gồm Mã phòng ban (DepartmentID), tên phòng ban (name), Lương trung bình (AvgofRate). Dữ liệu từ các bảng [HumanResources].[Department], [HumanResources].[EmployeeDepartmentHistory], [HumanResources].[EmployeePayHistory].
+select hrd.DepartmentID, hrd.Name, AvgofRate = avg(heph.Rate)
+from HumanResources.Department hrd
+	join HumanResourcess.EmployeeDepartmentHistory hedh on hrd.DepartmentID = hedh.DepartmentID
+	join HumanResourcess.EmployeePayHistory heph on hedh.BusinessEntityID = heph.BusinessEntityID
+group by hrd.DepartmentID, hrd.Name
+having avg(heph.Rate) > 30
